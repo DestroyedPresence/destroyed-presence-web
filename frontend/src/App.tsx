4cecb20aca7e3b0d67d2065353e248b04ffc033b
Abdulrahman Albalawi <d777mapps@gmail.com>
2025-02-23 20:19:13 -0500

    lab3 completion

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,8 @@ import {BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
 import Home from "./Home/Home";
 import Products from "./Products/Products";
 import Orders from "./Orders/Orders";
+import OrderDetail from "./Orders/OrderDetails";
+import Payment from "./Payment/Payment";
 
 function App() {
   const openMenu = () =>{
@@ -34,6 +36,7 @@ function App() {
                       <Link to="/">Home</Link>
                       <Link to="/catalog">Catalog</Link>
                       <Link to="/orders">Orders</Link>
+                      <Link to="/payment">Payment</Link>
                     </div>
                     
                 </header>   
@@ -59,6 +62,7 @@ function App() {
               <Route path="/catalog" element={<Products />} />
               <Route path="/orders" element={<Orders />} />
               <Route path="/orders/:id" element={<OrderDetail />} />
+              <Route path="/payment" element={<Payment />} />
             </Routes>
         </main>
 
